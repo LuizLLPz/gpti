e3b467b344417544735746f242d427cea706773a
luizllpz <luizllp321@gmail.com>
2023-06-17 23:08:04 -0300

    feat: adicionado label no input

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -2,6 +2,7 @@ import {FieldValues, Path, UseFormRegister} from "react-hook-form";
 
 type InputProps<T> = {
     name: Path<T extends FieldValues ? any : any>;
+    label: string
     placeholder: string
     register: UseFormRegister<T extends FieldValues ? any : any>;
     type?: React.InputHTMLAttributes<T>
@@ -10,7 +11,7 @@ type InputProps<T> = {
 export function Input<T>({name, placeholder, register, type}: InputProps<T>): JSX.Element {
     return (
         <div className="d-flex w-25 gap-2">
-            <label htmlFor={name}>Nome</label>
+            <label htmlFor={name}>{label}</label>
             <input type="text" id={name} {...register(name)}/>
         </div>);
 }
