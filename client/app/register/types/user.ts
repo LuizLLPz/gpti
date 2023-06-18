a3e36f06d89433b742cdf1d61cff4dd1ad8729d5
luizllpz <luizllp321@gmail.com>
2023-06-17 22:40:33 -0300

    feat: adicionado modelo para usuario de form e de back

---
+++ client/app/register/types/user.ts
@@ -0,0 +1,13 @@
+export type User = {
+    username: string;
+    name?: string;
+    surname?: string;
+    password: string;
+}
+
+export type UserDTO = {
+    NOMEUSUARIO: string;
+    NOME?: string;
+    SOBRENOME?: string;
+    SENHA: string;
+}
