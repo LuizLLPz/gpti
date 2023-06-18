6ff3721aaefa62a5871e63803bd0339396bc6a84
luizllpz <luizllp321@gmail.com>
2023-06-18 14:14:26 -0300

    feat: correções projeto + ajustes tela salvar projeto

--- client/app/dashboard/projeto/page.tsx
+++ client/app/dashboard/projeto/page.tsx
@@ -1,12 +1,36 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { Project } from "../../layout";
+import { Button, Input } from "@/components";
 
 import Header from "../../header";
+import {useForm} from "react-hook-form";
+import {Project, ProjectDTO} from "@/app/dashboard/types";
 
 export default function Dashboard() {
     const [project, setProject] = useState<Project[]>([])
+
+    const {register, handleSubmit} = useForm<Project>();
+
+    async function submitProject(model: Project) {
+        const project: ProjectDTO = {
+            IDEMPRESA: 1,
+            NOME: model.name,
+            DESCRICAO: model.description
+        }
+        const data = await fetch('https://7f69-191-243-137-66.ngrok-free.app/Projeto/criarProjeto?XDEBUG_SESSION_START=11465',
+            {body: JSON.stringify(project), method: 'POST'})
+        const json = await data.json();
+        if (data.status < 400) {
+            alert(json.message)
+            setTimeout(() => {
+                window.location.href = "/dashboard", 2000
+            })
+        }
+        else {
+            alert(json.message)
+        }
+    }
 
     useEffect(
         () => {
@@ -21,14 +45,17 @@ export default function Dashboard() {
         <main className="container">
         <form>
             <div className="mb-3">
-                <label htmlFor="name" className="form-label">Nome</label>
-                <input type="text" className="form-control" id="name"/>
+                <Input name="name" label="Nome:" placeholder="" register={register}/>
+                {/*<label htmlFor="name" className="form-label">Nome</label>*/}
+                {/*<input type="text" className="form-control" id="name"/>*/}
             </div>
             <div className="mb-3">
-                <label htmlFor="description" className="form-label">Descrição</label>
-                <input type="text" className="form-control" id="description"/>
+                <Input name="description" label="Descrição:" placeholder="" register={register}/>
+                {/*<label htmlFor="description" className="form-label">Descrição</label>*/}
+                {/*<input type="text" className="form-control" id="description"/>*/}
             </div>
-            <button type="submit" className="btn btn-primary">Adicionar</button>
+            <Button text="Adicionar" onClick={handleSubmit(submitProject)}/>
+            {/*<button type="submit" className="btn btn-primary">Adicionar</button>*/}
 </form>
 
         </main>
